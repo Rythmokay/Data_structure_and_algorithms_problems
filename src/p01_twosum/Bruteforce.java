c6d79fc6f749be30d54af889ebad08ea58a23840
Rythm Jagga <rythm.okay@gmail.com>
2026-02-01 23:10:27 +0530

    two sum all approaches

--- src/p01_twosum/Bruteforce.java
+++ src/p01_twosum/Bruteforce.java
@@ -1,5 +1,17 @@
 package p01_twosum;
 
 public class Bruteforce {
+    public int[] twoSum(int[] nums, int target) {
+        // just checking the brute force approach
+        for (int i =0;i<nums.length;i++){
+            for (int j =i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    return new int[] {i,j};
 
+                }
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
 }
---
+++ src/p01_twosum/Optimal.java
@@ -0,0 +1,18 @@
+package p01_twosum;
+import java.util.*;
+public class Optimal {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap <Integer, Integer > map = new HashMap<> ();
+            int n = nums.length;
+            for (int i =0;i<n;i++){
+                int complement = target - nums[i];
+                if (map.containsKey(complement)){
+                    return new int[] {map.get(complement),i};
+
+                }
+                map.put(nums[i],i);
+            }
+            return new int [] {-1,-1};
+        }
+
+}
